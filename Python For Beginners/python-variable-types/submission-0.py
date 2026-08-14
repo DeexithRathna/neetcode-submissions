50bac56ac1b153103e4705ac4ef59d7a980b4d11
Deexith Rathna <deexith.reddy@gmail.com>
2026-08-14 14:39:12 +0530

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,10 @@
+integer_type = 25
+float_type = 0.5
+bool_type = True
+string_type = "Hello"
+list_type = ["Alice"]
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
